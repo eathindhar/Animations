1ab003912a78c744babe036d4ea3bd00925828b5
Eathindhar Mahendradevan <eathindhar@gmail.com>
2019-06-17 18:33:09 +0530

    Final Commit

--- app/src/main/java/com/eathindhar/animations/MainActivity.java
+++ app/src/main/java/com/eathindhar/animations/MainActivity.java
@@ -1,6 +1,5 @@
 package com.eathindhar.animations;
 
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
